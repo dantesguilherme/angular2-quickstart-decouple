ccb5bc5be93456de90659362d9b2693314afab16
Dantes Guilherme <dantesguilherme@gmail.com>
2016-09-22 18:14:41 -0300

    added bootstrap css with ng2-bootstrap

--- angular2/app/app.component.ts
+++ angular2/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><alert type="info">ng2-bootstrap hello world!</alert>'
 })
 export class AppComponent { }
--- docs/app/app.component.js
+++ docs/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: '<h1>My First Angular 2 App</h1><alert type="info">ng2-bootstrap hello world!</alert>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
