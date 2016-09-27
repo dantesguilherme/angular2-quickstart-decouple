57799477484d454454620975ae8722a369ca3b41
Dantes Guilherme <dantesguilherme@gmail.com>
2016-09-27 18:29:38 -0300

    publish demo

---
+++ wwwroot/app/admin/admin.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+var admin_component_1 = require('./admin.component');
+exports.MODULE_ROUTES = [
+    { path: 'admin', component: admin_component_1.AdminComponent }
+];
+exports.MODULE_COMPONENTS = [
+    admin_component_1.AdminComponent
+];
+//# sourceMappingURL=admin.routes.js.map
